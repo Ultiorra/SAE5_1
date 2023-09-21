86730f3e29b42c85a1cda1673963a8a4960418c6
AlexisFl <alexis_falconnet@etu.u-bourgogne.fr>
2023-09-21 12:48:17 +0200

    Board

---
+++ src/ChessBoard.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Chessboard } from 'react-chessboard';
+import {Chess} from 'chess.js';
+
+const MyChessboard = () => {
+    const [chess] = useState(new Chess());
+
+    const handleDrop = (sourceSquare, targetSquare) => {
+        const move = chess.move({
+            from: sourceSquare,
+            to: targetSquare,
+            promotion: "q"
+        });
+
+        if (move === null) return;
+
+        // make random legal move for black
+        window.setTimeout(() => {
+            const moves = chess.moves();
+            const move = moves[Math.floor(Math.random() * moves.length)];
+            chess.move(move);
+            setFen(chess.fen());
+        }, 500);
+    };
+
+    return (
+        <Chessboard
+            position={chess.fen()}
+            onDrop={(sourceSquare, targetSquare) => handleDrop(sourceSquare, targetSquare)}
+        />
+    );
+};
+
+export default MyChessboard;
