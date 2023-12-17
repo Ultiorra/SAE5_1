1e0e29a0d95fb400bd26a563fffa1f874cc14b7e
af303746 <alexis_falconnet@etu.u-bourgogne.fr>
2023-12-17 11:12:03 +0100

    Reconnais les bon coups mais ne joue que le 1er en auto

--- Front/src/Components/DirectoriesBoard.js
+++ Front/src/Components/DirectoriesBoard.js
@@ -10,19 +10,26 @@ const DirectoriesBoard = () => {
         const parsedMoves = [];
 
         for (let i = 0; i < moves.length; i++) {
-            const currentMove = (i === 0) ? moves[i] : `1. ${moves.slice(0, i + 1).join(' ')}`;
-            parsedMoves.push(currentMove);
+            let currentMove = (i === 0) ? moves[i] : `1. ${moves.slice(0, i + 1).join(' ')}`;
+
+            if (Array.isArray(currentMove)) {
+                for (let j = 0; j < currentMove.length; j++) {
+                    const move = currentMove[j];
+                    parsedMoves.push(move);
+                }
+            } else {
+                parsedMoves.push(currentMove);
+            }
         }
 
         return parsedMoves;
     }
 
-    const { value } = useParams();
 
-    const {userColor} = "w";
+    const { value } = useParams();
+    const { userColor } = "w";
 
     const [tempChess, setTempChess] = useState(new Chess());
-
     const [pgn, setPgn] = useState("");
     const [chess, setChess] = useState(new Chess());
     const [chessHistory, setChessHistory] = useState([new Chess()]);
@@ -37,46 +44,49 @@ const DirectoriesBoard = () => {
             const updatedChessCopy = new Chess(chess.fen());
             setUpdatedChessCopy(updatedChessCopy);
 
-
-
-            /*if (updatedChessCopy.turn() === "b") {
-                console.log(updatedChessCopy.moveNumber());
-                const newChess = new Chess();
-                newChess.loadPgn(parsedMoves[updatedChessCopy.moveNumber() - 1], { newlineChar: ':' });
-                console.log(parsedMoves[updatedChessCopy.moveNumber() - 1]);
-                setUpdatedChessCopy(newChess);
-                setTempChess(newChess);
-                return;
-            }*/
-
             const move = updatedChessCopy.move({
                 from: sourceSquare,
                 to: targetSquare,
                 promotion: "q"
             });
 
-            const newChess = new Chess();
+            let newChess = new Chess();
             newChess.loadPgn(pgn, { newlineChar: ':' });
             newChess.move(move);
             setTempChess(newChess);
 
-            console.log("tempChess");
-            console.log(tempChess);
-            console.log(tempChess.pgn());
-
             if (move !== null) {
-                if (parsedMoves[chess.moveNumber() - 1].includes(move.san)) {
-                    console.log(chess.moveNumber());
-                    const newChess = new Chess();
-                    newChess.loadPgn(parsedMoves[chess.moveNumber() - 1], {newlineChar: ':'});
-                    console.log(parsedMoves[chess.moveNumber() - 1]);
-                    setUpdatedChessCopy(newChess);
-                    setTempChess(newChess);
+                const currentMove = parsedMoves[chess.moveNumber() - 1];
+                if (!currentMove.includes(move.san)) {
+                    console.log("Coup invalide");
                     return;
                 }
 
+                let updatedChessHistory = chessHistory.slice(0, currentMoveIndex + 1);
+                updatedChessHistory.push(newChess);
+                setPgn(newChess.pgn());
+                setChess(newChess);
+                setChessHistory(updatedChessHistory);
+                setCurrentMoveIndex(currentMoveIndex + 1);
+
+
+                const opponentMove = parsedMoves[chess.moveNumber() - 1];
+                console.log("opponentMove")
+                console.log(opponentMove);
+                console.log("chess.moveNumber()")
+                newChess = new Chess();
+                newChess.loadPgn(opponentMove);
+                updatedChessHistory = chessHistory.slice(0, currentMoveIndex + 1);
+                updatedChessHistory.push(newChess);
+                setTempChess(newChess);
+                setPgn(newChess.pgn());
+                setChess(newChess);
+                setChessHistory(updatedChessHistory);
+                setCurrentMoveIndex(currentMoveIndex + 1);
+
+
             } else {
-                console.log("Invalid move");
+                console.log("Coup invalide");
                 return;
             }
         } catch (e) {
@@ -84,28 +94,14 @@ const DirectoriesBoard = () => {
         }
     }
 
-    useEffect(() => {
-        if (value.includes(tempChess.pgn())) {
-            const updatedChessHistory = chessHistory.slice(0, currentMoveIndex + 1);
-            updatedChessHistory.push(updatedChessCopy);
-
-            setPgn(tempChess.pgn());
-            setChess(updatedChessCopy);
-
-            setChessHistory(updatedChessHistory);
-            setCurrentMoveIndex(currentMoveIndex + 1);
-        } else {
-            console.log("Invalid position");
-            return;
-        }
-    } , [tempChess]);
-
     return (
         <div>
-            <Chessboard
-                position={chess.fen()}
-                onPieceDrop={(sourceSquare, targetSquare) => handleDrop(sourceSquare, targetSquare)}
-            />
+            <div className="board">
+                <Chessboard
+                    position={chess.fen()}
+                    onPieceDrop={(sourceSquare, targetSquare) => handleDrop(sourceSquare, targetSquare)}
+                />
+            </div>
             <p>PGN</p>
             <p>{pgn}</p>
             <p>VALUE</p>
